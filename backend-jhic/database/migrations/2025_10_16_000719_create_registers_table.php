1bf1c28777357159977996f6c8dfb3a3761978e3
robotganteng09 <arsyafaisyar@gmail.com>
2025-10-16 10:57:42 +0700

    FEAT[AMF] buat database,model,dan controller

---
+++ backend-jhic/database/migrations/2025_10_16_000719_create_registers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_siswa');
+            $table->string('nama_walmur');
+            $table->integer('nik')->unique();
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registers');
+    }
+};
